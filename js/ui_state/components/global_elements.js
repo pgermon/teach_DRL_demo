fe9cafa5a211f9fabd1adeddf994a1d9e3e6a446
pgermon <pgermon@users.noreply.github.com>
2021-07-12 12:20:28 +0000

    deploy: 42d8d4bccfbc3dfaefb3ca53a7977dc21511371c

--- js/ui_state/components/global_elements.js
+++ js/ui_state/components/global_elements.js
@@ -27,10 +27,10 @@ export default class GlobalElements extends Component{
         this.element.querySelector('#demoTitle').innerText = dict['demoTitle'];
 
         // Tabs buttons
-        this.element.querySelector('#getting-started-btn').innerHTML = dict['gettingStarted'];
-        this.element.querySelector('#parkour-custom-btn').innerHTML = dict['parkourCustomization'];
-        this.element.querySelector('#advanced-options-btn').innerHTML = dict['advancedOptions'];
-        this.element.querySelector('#about-drl-btn').innerHTML = dict['aboutDeepRL'];
+        this.element.querySelector('#getting-started-btn').innerText = dict['gettingStarted'];
+        this.element.querySelector('#parkour-custom-btn').innerText = dict['parkourCustomization'];
+        this.element.querySelector('#advanced-options-btn').innerText = dict['advancedOptions'];
+        this.element.querySelector('#about-drl-btn').innerHTML = `<i class="fas fa-info-circle fa-lg"></i> ${dict['aboutDeepRL']}`;
 
         // Changes the selected index of the language dropdown
         this.element.querySelector('#langSelect').selectedIndex = store.state.language == 'EN' ? 0 : 1;
